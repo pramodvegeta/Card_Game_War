243f6d781e61709c47f9e2fbccf11f30c4bf47a3
Pramod Chandra <chivukup@sheridancollege.ca>
2019-06-09 16:52:41 -0400

    Test2 Commit on Pramod

--- SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Card.java
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Card.java
@@ -2,7 +2,7 @@
  * SYST 17796 Project Winter 2019 Base code.
  * Students can modify and extend to implement their game.
  * Add your name as a modifier and the date!
- * @modifier Pramod1
+ * @modifier Pramod2
  */
 package ca.sheridancollege.project;
 
