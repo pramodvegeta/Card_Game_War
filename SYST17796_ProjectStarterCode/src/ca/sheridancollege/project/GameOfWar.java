18e1e3688d1ca6243c7bb6b941a303f0c0930aea
Pramod Chandra <50593544+pramodvegeta@users.noreply.github.com>
2019-08-12 18:03:31 -0400

    Updating GameOfWar

--- SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/GameOfWar.java
+++ SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/GameOfWar.java
@@ -9,7 +9,6 @@ public class GameOfWar extends Game {
     GroupOfCards deck = new GroupOfCards(52);
 
     int roundNum = 0;
-    boolean war = false;
     int warNum = 0;
 
     PlayerOfWar p1;
@@ -25,85 +24,68 @@ public class GameOfWar extends Game {
         deck.shuffle();
         p1 = (PlayerOfWar) getPlayers().get(0);
         p2 = (PlayerOfWar) getPlayers().get(1);
-
         deck.deal(p1.getPile(), p2.getPile());
     }
 
     @Override
     public void declareWinner() {
-        System.out.print("winner is: ");
         if (p1.getPile().showCards().isEmpty() || p1.getPile().showCards().size() < 3) {
-            System.out.println(p2.getPlayerID());
+            System.out.println("You have lost the game");
         } else {
-            System.out.println(p1.getPlayerID());
+            System.out.println("You have won the game");
         }
 
     }
 
     public String oneRound() {
-        System.out.println("Round number: " + roundNum);
-        System.out.println("Pile 1 is " + p1.getPile().showCards().size());
-        System.out.println("Pile 2 is " + p2.getPile().showCards().size());
-        p1.play();
-
+       p1.play();
         p2.play();
         roundNum++;
-        return declareResult(p1.flipACard(), p2.flipACard(), false);
+        return declareResult(p1.flipACard(0), p2.flipACard(0), false);
     }
 
     public String declareResult(Card c1, Card c2, boolean war) {
         String roundWinner = "";
-        int c = 0;
         int result = ((CardOfWar) c1).compareTo((CardOfWar) c2);
         switch (result) {
             case -1:
-                warNum = 0;
                 if (war) {
+                    warNum = 0;
                     for (int i = 0; i < 3; i++) {
-                        p2.getPile().showCards().add(p1.removeACard(0));
-                        p2.getPile().showCards().add(p2.removeACard(0));
-
-                        System.out.println("-1 War if " + c);
+                        p2.getPile().showCards().add(p1.removeACard());
+                        p2.getPile().showCards().add(p2.removeACard());
                     }
-                    System.out.println("-1 if c is: " + c);
                 } else {
-                    p2.getPile().showCards().add(p1.removeACard(0));
-                    p2.getPile().showCards().add(p2.removeACard(0));
-                    System.out.println("-1 else c is: " + c);
+                    p2.getPile().showCards().add(p1.removeACard());
+                    p2.getPile().showCards().add(p2.removeACard());
                 }
-                System.out.println("-1 c is: " + c);
                 roundWinner = "lost";
                 break;
             case 0:
                 roundWinner = "tie";
                 if (p2.getPile().showCards().size() < 3) {
-                    System.out.println("0 if c is: " + c);
-                    isWinner();
+                    declareWinner();
 
                 } else if (p1.getPile().showCards().size() < 3) {
-                    System.out.println("0 else if c is: " + c);
-                    isWinner();
+                    declareWinner();
 
                 } else {
-                    System.out.println("0 else c is: " + c);
                     warNum = warNum + 2;
                     war();
                 }
+
                 break;
             case 1:
-                warNum = 0;
                 if (war) {
+                    warNum = 0;
                     for (int i = 0; i < 3; i++) {
-                        p1.getPile().showCards().add(p2.removeACard(0));
-                        p1.getPile().showCards().add(p1.removeACard(0));
-                        System.out.println("1 if c is: " + c);
+                        p1.getPile().showCards().add(p2.removeACard());
+                        p1.getPile().showCards().add(p1.removeACard());
                     }
                 } else {
-                    p1.getPile().showCards().add(p2.removeACard(0));
-                    p1.getPile().showCards().add(p1.removeACard(0));
-                    System.out.println("1 else c is: " + c);
+                    p1.getPile().showCards().add(p2.removeACard());
+                    p1.getPile().showCards().add(p1.removeACard());
                 }
-                System.out.println("1 c is: " + c);
                 roundWinner = "won";
                 break;
         }
@@ -114,9 +96,14 @@ public class GameOfWar extends Game {
     public void war() {
         declareResult(p1.flipACard(warNum), p2.flipACard(warNum), true);
     }
+    
+    
+    public int getWarNum() {
+        return warNum;
+    }
 
     public boolean isWinner() {
-        if (p1.getPile().showCards().size() <= 2 || p1.getPile().showCards().size() >= 50 || p2.getPile().showCards().size() >= 50 || p2.getPile().showCards().size() <= 2) {
+        if (p1.getPile().showCards().size() <= 2 || p1.getPile().showCards().size() >= 50) {
             declareWinner();
             return false;
         } else {
