823bf5cfb16a775ffa371b4fe7ec17d97ba872f7
Pramod Chandra <50593544+pramodvegeta@users.noreply.github.com>
2019-08-12 22:41:40 -0400

    Final JUnitTesting for GameOfWar

--- GameOfWarTest.java
+++ GameOfWarTest.java
@@ -5,7 +5,6 @@
  */
 package ca.sheridancollege.project;
 
-
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.BeforeClass;
@@ -15,9 +14,8 @@ import org.junit.BeforeClass;
  * @author Kiran
  */
 public class GameOfWarTest {
-    
 
-     GameOfWar gow = new GameOfWar("War");
+    GameOfWar gow = new GameOfWar("War");
 
     public GameOfWarTest() {
         PlayerOfWar p1 = new PlayerOfWar("name");
@@ -27,37 +25,59 @@ public class GameOfWarTest {
         gow.play();
     }
 
+    @BeforeClass
+    public static void setUpClass() {
+        System.out.println("Before Class");
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        System.out.println("After Class");
+    }
+
+    @Before
+    public void setUp() {
+        System.out.println("Before Test");
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("After Class");
+    }
+
     /**
-     * Test of declareResult method, of class GameOfWar.
+     * Test of play method, of class GameOfWar.
      */
     @Test
     public void testGoodDeclareResult() {
         System.out.println("declareResult");
-        CardOfWar c1 = new CardOfWar("TWO","SPADE");
-        CardOfWar c2 = new CardOfWar("ACE","HEART");
+        CardOfWar c1 = new CardOfWar("TWO", "SPADE");
+        CardOfWar c2 = new CardOfWar("ACE", "HEART");
         String expResult = "won";
         String result = gow.declareResult(c1, c2, true);
         assertEquals(expResult, result);
-        
+
     }
+
     @Test
     public void testBadDeclareResult() {
         System.out.println("declareResult");
-        CardOfWar c2 = new CardOfWar("TWO","SPADE");
-        CardOfWar c1 = new CardOfWar("ACE","HEART");
+        CardOfWar c2 = new CardOfWar("TWO", "SPADE");
+        CardOfWar c1 = new CardOfWar("ACE", "HEART");
         String expResult = "lost";
         String result = gow.declareResult(c1, c2, true);
         assertEquals(expResult, result);
-        
+
     }
+
     @Test
     public void testBoundaryDeclareResult() {
         System.out.println("declareResult");
-        CardOfWar c1 = new CardOfWar("ACE","HEART");
-        CardOfWar c2 = new CardOfWar("ACE","HEART");
+        CardOfWar c1 = new CardOfWar("ACE", "HEART");
+        CardOfWar c2 = new CardOfWar("ACE", "HEART");
         String expResult = "tie";
         String result = gow.declareResult(c1, c2, true);
         assertEquals(expResult, result);
     }
-    
+
 }
