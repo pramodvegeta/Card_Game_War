56c2fd3937203f36f4019c23323b9a1955e6c10c
Judd <44623501+ComfortablyCoding@users.noreply.github.com>
2019-08-12 21:34:17 -0400

    Removed unnecessary files
    Removed files that do not belong on the branch.

--- CardOfWarTest.java
+++ CardOfWarTest.java
@@ -16,44 +16,36 @@ public class CardOfWarTest {
     
     public CardOfWarTest() {
     }
-    
-    
 
     /**
      * Test of toString method, of class CardOfWar.
      */
      @Test
     public void testGoodCompareTo() {
-        System.out.println("compareTo");
-//        CardOfWar card = null;
+        System.out.println("Testing the compareTo method");
         CardOfWar instance1 = new CardOfWar("TWO","SPADE");
         CardOfWar instance2 = new CardOfWar("ACE","HEART");
         int expResult = 1;
         int result = instance1.compareTo(instance2);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
     }
     @Test
     public void testBoundaryCompareTo() {
-        System.out.println("compareTo");
-//        CardOfWar card = null;
+        System.out.println("Testing the compareTo method");
         CardOfWar instance1 = new CardOfWar("ACE","SPADE");
         CardOfWar instance2 = new CardOfWar("ACE","HEART");
         int expResult = 0;
         int result = instance1.compareTo(instance2);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
     }
      @Test
     public void testBadCompareTo() {
-        System.out.println("compareTo");
-//        CardOfWar card = null;
+        System.out.println("Testing the compareTo method");
         CardOfWar instance1 = new CardOfWar("ACE","SPADE");
         CardOfWar instance2 = new CardOfWar("TWO","HEART");
         int expResult = -1;
         int result = instance1.compareTo(instance2);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
     }
     
 }
--- GameOfWarTest.java
+++ GameOfWarTest.java
@@ -16,26 +16,16 @@ import org.junit.BeforeClass;
  */
 public class GameOfWarTest {
     
+
+     GameOfWar gow = new GameOfWar("War");
+
     public GameOfWarTest() {
         PlayerOfWar p1 = new PlayerOfWar("name");
         PlayerOfWar p2 = new PlayerOfWar("Computer");
-//        PlayerOfWar tie = new PlayerOfWar("tie");
         gow.getPlayers().add(p1);
         gow.getPlayers().add(p2);
-//        gow.getPlayers().add(tie);
         gow.play();
     }
-     GameOfWar gow = new GameOfWar("War");
- 
-//@BeforeClass
-//        public static void check() {
-//           
-       
-   
-
-   
-
-  
 
     /**
      * Test of declareResult method, of class GameOfWar.
@@ -57,8 +47,6 @@ public class GameOfWarTest {
         System.out.println("declareResult");
         CardOfWar c2 = new CardOfWar("TWO","SPADE");
         CardOfWar c1 = new CardOfWar("ACE","HEART");
-       // boolean war = true;
-       // GameOfWar instance = new GameOfWar("");
         String expResult = "lost";
         String result = gow.declareResult(c1, c2, true);
         assertEquals(expResult, result);
@@ -69,26 +57,9 @@ public class GameOfWarTest {
         System.out.println("declareResult");
         CardOfWar c1 = new CardOfWar("ACE","HEART");
         CardOfWar c2 = new CardOfWar("ACE","HEART");
-       // boolean war = true;
-       // GameOfWar instance = new GameOfWar("");
         String expResult = "tie";
         String result = gow.declareResult(c1, c2, true);
         assertEquals(expResult, result);
-        
     }
-
-
-    /**
-     * Test of isWinner method, of class GameOfWar.
-     */
-//    @Test
-//    public void testIsWinner() {
-//        System.out.println("isWinner");
-//        GameOfWar instance = new GameOfWar("true") ;
-//        boolean expResult = "";
-//        boolean result = instance.isWinner();
-//        assertEquals(expResult, result);
-//        
-//    }
     
 }
